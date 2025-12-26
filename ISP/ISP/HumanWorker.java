5430bc3a5bca29547a228b2a4193ab271acda834
JAC2005 <mangouojacky01@gmail.com>
2025-12-26 00:17:16 -0800

    (ISP): ajouter HumanWorker (implémente Worker)

---
+++ ISP/ISP/HumanWorker.java
@@ -0,0 +1,11 @@
+public class HumanWorker implements Worker {
+    @Override
+    public void work() {
+        System.out.println("Humain travaille");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Humain mange");
+    }
+}
