e9d2c93801c018ec9dcfa5e635d3c98d10c751d3
JAC2005 <mangouojacky01@gmail.com>
2025-12-25 23:48:16 -0800

    (OCP): ajouter CircleAvant (avant refactoring)

---
+++ OCP/OCP/OCP/CircleAvant.java
@@ -0,0 +1,9 @@
+public class CircleAvant {
+    private double radius;
+
+    public CircleAvant(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() { return radius; }
+}
