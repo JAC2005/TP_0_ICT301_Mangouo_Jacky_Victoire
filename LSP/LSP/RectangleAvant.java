22d823418ec4090df28c3bea77f23211960e7f98
JAC2005 <mangouojacky01@gmail.com>
2025-12-26 00:07:38 -0800

    (LSP): ajouter RectangleAvant (avant refactoring)

---
+++ LSP/LSP/RectangleAvant.java
@@ -0,0 +1,16 @@
+public class RectangleAvant {
+    protected double width;
+    protected double height;
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
