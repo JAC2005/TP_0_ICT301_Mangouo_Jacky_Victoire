f0d2778d7a5072aba85cdf96cc0f781a8c244c7d
JAC2005 <mangouojacky01@gmail.com>
2025-12-25 23:55:23 -0800

    (OCP): ajouter RectangleAvant (avant refactoring)

---
+++ OCP/OCP/RectangleAvant.java
@@ -0,0 +1,12 @@
+public class RectangleAvant {
+    private double width;
+    private double height;
+
+    public RectangleAvant(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+}
