ad2eb484cc346459538eacf5e6d7ba0115b431f0
JAC2005 <mangouojacky01@gmail.com>
2025-12-25 23:50:18 -0800

    Reinitialisation

--- OCP/OCP/OCP/CircleAvant.java
+++
@@ -1,9 +0,0 @@
-public class CircleAvant {
-    private double radius;
-
-    public CircleAvant(double radius) {
-        this.radius = radius;
-    }
-
-    public double getRadius() { return radius; }
-}
